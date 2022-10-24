fcbfdd80b6ba449d0d8404ffe2cf1075281315cd
josepk01 <72452701+josepk01@users.noreply.github.com>
2022-10-24 10:12:23 +0200

    sprite angel metido
    Falta cuadrar y que no se caiga

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -1,31 +1,30 @@
-export default class Boot extends Phaser.Scene 
-{
+export default class Boot extends Phaser.Scene {
     /**
      * Constructor de la escena
      */
     constructor() {
-      super({
-        key: 'boot'
-      });
+        super({
+            key: 'boot'
+        });
     }
 
-    preload() 
-    {
+    preload() {
         console.log("Boot scene")
 
 
         // --- PLAYER --- 
         // player spritesheet
-        //this.load.spritesheet('jetpac', "./assets/sprites/sprites_angel_original_y_escalado/angel3.png", //cambiar el sprite para la animacion 
-        this.load.spritesheet('jetpac', "./assets/sprites/jetpac.png", //cambiar el sprite para la animacion 
-        { frameWidth: 17, frameHeight: 24 }) 
+        this.load.spritesheet('angel', "./assets/sprites/angel3.png", //cambiar el sprite para la animacion 
+            { frameWidth: 91, frameHeight: 72 })
+        /*this.load.spritesheet('jetpac', "./assets/sprites/jetpac.png", //cambiar el sprite para la animacion 
+            { frameWidth: 17, frameHeight: 24 })*/
 
-        // jetpack audio
-        this.load.audio('jetpack', './assets/sounds/jetpack2.wav')//cambio mas tarde
-    
-        // walking audio
-        this.load.audio('walk-audio', './assets/sounds/soldier-walk.wav')//cambio mas tarde
-        
+        // // jetpack audio
+        // this.load.audio('jetpack', './assets/sounds/jetpack2.wav')//cambio mas tarde
+
+        // // walking audio
+        // this.load.audio('walk-audio', './assets/sounds/soldier-walk.wav')//cambio mas tarde
+
 
         // --- SCENE --- 
         // platform
@@ -49,12 +48,12 @@ export default class Boot extends Phaser.Scene
 
         /*Todo esto para ataques a distancia*/
         // bullet spritesheet
-        this.load.spritesheet('bullet', "./assets/sprites/meteor.png", 
-            { frameWidth: 16, frameHeight: 14 }) 
-        
+        this.load.spritesheet('bullet', "./assets/sprites/meteor.png",
+            { frameWidth: 16, frameHeight: 14 })
+
         // explosion spritesheet
-        this.load.spritesheet('explosion', "./assets/sprites/explosion.png", 
-            { frameWidth: 24, frameHeight: 17 }) 
+        this.load.spritesheet('explosion', "./assets/sprites/explosion.png",
+            { frameWidth: 24, frameHeight: 17 })
 
         // explode audio
         this.load.audio('explode', './assets/sounds/explosion.wav')
@@ -72,41 +71,40 @@ export default class Boot extends Phaser.Scene
 
     }
 
-    create() 
-    {      
+    create() {
         // --- PLAYER --- cuadriplicar para cada cosa que se pilla
         // creates walk animation for player
         this.anims.create({
             key: 'walk',
-            frames: this.anims.generateFrameNames('jetpac', { start: 4, end: 7 }),
+            frames: this.anims.generateFrameNames('angel', { start: 4, end: 7 }),
             frameRate: 10,
             repeat: -1
         })
 
         // creates jump animation for player
         this.anims.create({
-          key: 'jump',
-          frames: this.anims.generateFrameNames('jetpac', { start: 0, end: 3 }),
-          frameRate: 10,
-          repeat: -1
+            key: 'jump',
+            frames: this.anims.generateFrameNames('angel', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
         })
 
 
         // --- BULLET --- duplicar esto disparo enemigo y disparo player
         // creates bullet animation 
         this.anims.create({
-          key: 'fly-bullet',//cambio para decir que es municion
-          frames: this.anims.generateFrameNames('bullet', { start: 0, end: 3 }),
-          frameRate: 6,
-          repeat: -1
+            key: 'fly-bullet',//cambio para decir que es municion
+            frames: this.anims.generateFrameNames('bullet', { start: 0, end: 3 }),
+            frameRate: 6,
+            repeat: -1
         })
 
         // creates bullet-explosion animation 
         this.anims.create({
-          key: 'explote',
-          frames: this.anims.generateFrameNames('explosion', { start: 0, end: 2 }),
-          frameRate: 5,
-          repeat: -1
+            key: 'explote',
+            frames: this.anims.generateFrameNames('explosion', { start: 0, end: 2 }),
+            frameRate: 5,
+            repeat: -1
         })
 
 
