6f6fcde00bcda9c32a7d560f111d1e91e48748cd
ACE D. POL <pablocub@ucm.es>
2022-11-17 16:57:39 +0100

    Barra de información

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -1,6 +1,8 @@
 import { gameLogic } from "../game.js"
+import { toggleInfoBar } from '../../lib/pauseCtrl.js'
 
 export function initGame(scene, lv) {
     // gameLogic.scene.getScene('menuGame').scene.start('pvliGame');
     scene.scene.start('pvliGame', lv);
+    toggleInfoBar();
 }
