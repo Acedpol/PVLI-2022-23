6977b7be8f03214fe67ef1f0605117683dbd2c2f
ACE D. POL <pablocub@ucm.es>
2022-11-26 01:43:25 +0100

    Update options.js
    Métodos para la mecánica de selección de controles.

--- src/scenes/options.js
+++ src/scenes/options.js
@@ -47,4 +47,60 @@ export default class Options extends blankMenu
     {
         super.update(t, dt);
     }
+
+    getRect(gameObject) {
+        let x = gameObject.x - gameObject.width * 0.5;
+        let y = gameObject.y - gameObject.height * 0.5;
+        return new Phaser.Geom.Rectangle(x, y, gameObject.width, gameObject.height);
+    }
+
+    btnContorno(gameObject, resize = true) {
+        let _rect = this.getRect(gameObject);
+        if (resize) _rect = this.resize(_rect);
+        return _rect;
+    }
+
+    resize(rect) {
+        let w = rect.width;
+        let h = rect.height;
+        let cw = 0.2;
+        let ch = 0.5;
+        rect.setSize(rect.width * (1 + cw), rect.height * (1 + ch));
+        rect.setPosition(rect.x - w * cw / 2, rect.y - h * ch / 2);
+        return rect;
+    }
+
+    activarA(scene) {
+        scene.graphicsA.setVisible(true);
+        scene.graphicsB.setVisible(false);
+        scene.optA = 1;
+        scene.optB = 3;
+    }
+    activarB(scene) {
+        scene.graphicsB.setVisible(true);
+        scene.graphicsA.setVisible(false);
+        scene.optA = 3;
+        scene.optB = 1;
+    }
+    apagarA(scene) {
+        scene.graphicsA.setVisible(false);
+        scene.optA = 1;
+    }
+    apagarB(scene) {
+        scene.graphicsB.setVisible(false);
+        scene.optB = 1;
+    }
+
+    colorear() {
+        this.optA ? this.activarA(this) : this.apagarA(this);
+        this.optB ? this.activarB(this) : this.apagarB(this);
+    }
+
+    // Para, B != 0 => !!B es igual a true;
+    colorear_v2() {
+        console.log("A: " + (this.optA === 3));
+        console.log("B: " + (this.optB === 3));
+        (this.optA === 3) ? this.activarA(this) : this.apagarA(this);
+        (this.optB === 3) ? this.activarB(this) : this.apagarB(this);
+    }
 }
