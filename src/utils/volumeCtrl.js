3210e6a81d61ab10b7832fb068f2d126e8b29407
ACE D. POL <pablocub@ucm.es>
2022-12-01 22:28:03 +0100

    Update volumeCtrl.js (slider creado)

--- src/utils/volumeCtrl.js
+++ src/utils/volumeCtrl.js
@@ -3,57 +3,51 @@
  * Si al añadir un objeto no tiene 'body' se hace estático por defecto.
  */
 
+import modulador from "./modulador.js";
+
 export default class volumeCtrl
 {
     /**
      * Constructor de enemigo
      * @param {Phaser.Scene} scene escena a la que pertenece
      */
-    constructor(scene)
+    constructor(scene, min, max, vertical = true)
     {
         this.scene = scene;
-        this.modulador();
+        this.vertical = vertical;
+        this.min = min;
+        this.max = max;
+
+        this.bar();
+        this.mod = new modulador(scene, min, max, vertical);
     }
 
     update(t, dt) {
 
     }
 
-    modulador() {
-        
+    bar() {
         const{width,height} = this.scene.scale;
-        let rw = 20;
-        let rh = 10;
+        let rw = 100 * 0.75;
+        let rh = 25 * 0.75;
 
-        let _rectStyle = { relleno: '0x000000', contorno: '0xffffff', alphaFill:  0, alphaLine:  0.85, drawFill: false, drawLine: true };
-        let _rect = new Phaser.Geom.Rectangle(width/2, height/2, rw, rh);
-        let _x = _rect.x + _rect.width * 0.5;
-        let _y = _rect.y + _rect.height * 0.5;
+        if (this.vertical) {
+            rw = 100 * 0.75;
+            rh = this.max - this.min;
+        } 
+        else {
+            rw = this.max - this.min;
+            rh = 100 * 0.75;
+        }
 
-        this.zone = this.scene.add.zone(_x, _y, _rect.width, _rect.height)
-            .setInteractive({
-                hitArea: _rect,
-                useHandCursor: true
-            })
-            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-                this.drag = true;;
-            })
-            .on(Phaser.Input.Events.POINTER_MOVE, () => {
-                if (this.drag) {
-                    // this.zone.x = this.scene.input.activePointer.x;
-                    this.zone.y = this.scene.input.activePointer.y;
-                    // _rect.x = this.scene.input.activePointer.x - _rect.width * 0.5;
-                    _rect.y = this.scene.input.activePointer.y - _rect.height * 0.5;
-                    this.graphics.clear();
-                    this.scene.resetRectDisplay(this.graphics, _rect, _rectStyle);
-                }
-            })
-            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, () => {
-                this.drag = false;
-            });
+        let _rectStyle1 = { relleno: '0x000033', contorno: '0xffffff', alphaFill:  0.85, alphaLine:  0.85, drawFill: true, drawLine: true };
+        let _fondo = new Phaser.Geom.Rectangle(width/2 + rw * 0.1, height/2  - rh * 0.5, rw * 0.8, rh);
 
-        
-        this.graphics = this.scene.setRectStyle(_rect, _rectStyle, 4);
+        let _rectStyle2 = { relleno: '0x0000ff', contorno: '0xffffff', alphaFill:  0.85, alphaLine:  0.85, drawFill: true, drawLine: true };
+        let _relleno = new Phaser.Geom.Rectangle(width/2 + rw * 0.1, height/2 + _fondo.height - rh * 0.3 - rh * 0.5, rw * 0.8, rh * 0.3);
+
+        this.graphics1 = this.scene.setRectStyle(_fondo, _rectStyle1, 2);
+        this.graphics2 = this.scene.setRectStyle(_relleno, _rectStyle2, 2);
     }
 
 }
