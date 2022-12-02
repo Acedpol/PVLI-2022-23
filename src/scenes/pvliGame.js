a6ee87a3244c93227935ca0b4e27267b458f2f27
jocarr01 <jocarr01@ucm.es>
2022-12-02 17:55:15 +0100

    Update pvliGame.js
    Se modifica la instancia de las alas

--- src/scenes/pvliGame.js
+++ src/scenes/pvliGame.js
@@ -67,7 +67,7 @@ export default class pvliGame extends blankGame
         // Crea un objeto para recoger en la escena
         this.addToScene(new Magic(this, 150, 150), true);
         this.addToScene(new Potion(this, 300, 100), true);
-		this.addToScene(new Wings(this, 300, 100));
+		this.addToScene(new Wings(this, 400, 400), true);
 
         // Sets the camera view
         this.startCamera({ width: this.mapWidth, height: this.mapHeight}); 
