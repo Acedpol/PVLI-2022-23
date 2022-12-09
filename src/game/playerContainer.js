8bc082ccc9cbdae7c9894bfa0542e43a98c49857
jocarr01 <82376126+jocarr01@users.noreply.github.com>
2022-12-09 12:22:44 +0100

    Update playerContainer.js

--- src/game/playerContainer.js
+++ src/game/playerContainer.js
@@ -1,7 +1,7 @@
-import Magic from "./magic.js"
-import Wings from "./wings.js"
-import Aura from "./aura.js"
-import PlayerLogic from "./player.js";
+import Magic from './magic.js'
+import Wings from './wings.js'
+import Aura from './aura.js'
+import PlayerLogic from './player.js'
 
 /** @type {Phaser.GameObjects.Container} */
 export default class PlayerContainer extends Phaser.GameObjects.Container
