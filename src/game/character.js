ab3227b0c6c71692798b555e15d6339c0d91d1a5
josepk01 <72452701+josepk01@users.noreply.github.com>
2022-12-12 19:10:29 +0100

    layers bien colocados

--- src/game/character.js
+++ src/game/character.js
@@ -19,6 +19,7 @@ export default class Character extends Entity
     constructor(scene, x, y, spritesheet, n = 0){
         super(scene, x, y, spritesheet, n);
         this.playerContainer = this.scene.playerContainer;
+        this.setDepth(3);
     }
 
     preUpdate(t,dt) 
