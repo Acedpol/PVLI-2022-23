7f2de399bf0b85fe962018b4b1ea4d2c89b2fb14
manuelpr <manuelpr@ucm.es>
2022-12-14 19:05:29 +0100

    arreglo tamaño deathzone

--- src/game/deathZone.js
+++ src/game/deathZone.js
@@ -1,25 +1,40 @@
-/** @type {Phaser.GameObjects.GameObject} */
-export default class DeadZone extends Phaser.Physics.Arcade.Sprite
+/* @type {Phaser.GameObjects.GameObject} */
+import Character from './character.js';
+export default class DeadZone extends Character
 {
     constructor(scene, x, y, wh, ht){
         super(scene, x, y,'');
         this.setDepth(0);
-        this.body.setSize(wh,ht,true);
-        this._player = this.playerContainer;
-        this._magic = this.playerContainer;
-        this._enemy = this.playerContainer;
+        //this.body.setSize(wh,ht,true);
+        this._player = this.scene.playerContainer;
+        this._magic = this.scene.playerContainer.magic;
+        //this._enemy = this.playerContainer;
+        this.init(wh,ht)
+    }
+
+    init(wh,ht)
+    {
+        this.timer = this.scene.time.addEvent({
+            delay: 200,
+            callback: sizeChange,
+            callbackScope: this
+        });
+
+        function sizeChange() {            
+            this.body.width = wh;
+            this.body.height = ht;
+        }
 
 
     }
-
     preUpdate(t,dt) 
     {
         this.checkPlayer();
         super.preUpdate(t,dt) // for animation and player detection (¡puede ser destruido!)
-        
+
     }
 
-    /**
+    /*
      * Método ir hacia el jugador (si lo toca le disminuye la vida)
      * @param {GameObject} target lo que persigue
      */
@@ -39,7 +54,7 @@ export default class DeadZone extends Phaser.Physics.Arcade.Sprite
     {
 
         if (this.scene.physics.overlap(this, this.scene.playerContainer))
-        {        
+        {
             this.scene.handleGameLose(this)
         }
 
@@ -48,4 +63,3 @@ export default class DeadZone extends Phaser.Physics.Arcade.Sprite
 
 
 }
-
