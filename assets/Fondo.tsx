eadbd90fa70f0296be6df8189a52bba72770a130
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-01-09 18:33:56 +0100

    assest y tsx fuera

--- assets/Fondo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.03.23" name="Fondo" tilewidth="16" tileheight="16" tilecount="630" columns="30">
- <image source="sprites/escenario/gothic-castle-background.png" width="480" height="336"/>
-</tileset>
--- assets/paredes.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="paredes" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="sprites/gothicvania patreon collection/gothicvania patreon collection/Gothic-Castle-Files/PNG/layers/gothic-castle-tileset.png" width="336" height="240"/>
-</tileset>
